b89503b773d561f5b7ce030175601e0c439f774b
Yi <wy7382@gmail.com>
2025-11-18 13:07:53 +1030

    Improve FerrisWheel component: optimize animation variants with useMemo, simplify animation logic, improve performance

--- src/components/FerrisWheel.js
+++ src/components/FerrisWheel.js
@@ -1,62 +1,79 @@
-import {motion} from 'framer-motion'
+import {motion} from 'framer-motion';
+import {useMemo} from 'react';
+
+const TECH_ICONS = [
+    "java.svg",
+    "jupyter.svg",
+    "mongodb.svg",
+    "python.svg",
+    "react-js.svg",
+    "springboot.svg",
+    "junit5.png",
+    "intelliJ.svg",
+    "angular.svg",
+    "typescript.svg"
+];
+
+const ANIMATION_CONFIG = {
+    itemDelay: 5,
+    rotationDuration: TECH_ICONS.length * 5,
+};
+
+const getTechName = (filename) => {
+    return filename.replace(/\.(svg|png)$/i, '').replace(/-/g, ' ');
+};
+
+const createAnimationVariants = (index) => {
+    const delay = index * ANIMATION_CONFIG.itemDelay;
+    
+    return {
+        initial: {
+            opacity: 0
+        },
+        animate: {
+            opacity: 1,
+            rotate: -360,
+            transition: {
+                opacity: {
+                    delay,
+                    duration: 0.3
+                },
+                rotate: {
+                    repeat: Infinity,
+                    duration: ANIMATION_CONFIG.rotationDuration,
+                    delay,
+                    ease: "linear"
+                }
+            }
+        }
+    };
+};
 
 export const FerrisWheel = () => {
-    const ferriesOfSelf = [
-        "java.svg",
-        "jupyter.svg",
-        "mongodb.svg",
-        "python.svg",
-        "react-js.svg",
-        "springboot.svg",
-        "junit5.png",
-        "intelliJ.svg",
-        "angular.svg",
-        "typescript.svg"
-    ]
-    return(
+    const animationVariants = useMemo(() => 
+        TECH_ICONS.map((_, index) => createAnimationVariants(index)),
+        []
+    );
+
+    return (
         <div>
-            {ferriesOfSelf.map((intro, index:number) => {
-                return (
-                        <motion.div
-                            className={"ferris-wheel-intro"}
-                            key={index+1}
-                            initial="initial"
-                            animate={['animate', 'initialHide',]}
-                            variants={{
-                                initial: {
-                                    opacity: 0
-                                },
-                                initialHide: {
-                                    opacity: 1,
-                                    transition: {
-                                        delay:index*5
-                                    }
-                                },
-                                animate: {
-                                    rotate: -360,
-                                    transition: {
-                                        repeat: Infinity,
-                                        duration: ferriesOfSelf.length*5,
-                                        delay:index*5,
-                                        ease: "linear"
-                                    }
-                                },
-                            }}
-                        >
-                            <div className="intro-parent">
-                                <img
-                                    src={require(`../assets/img/tech_icon/${intro}`)}
-                                    alt={intro}
-                                    className="intro-img"
-                                />
-                            </div>
-                        </motion.div>
-                    )
-                    
-            })}
-
+            {TECH_ICONS.map((iconFilename, index) => (
+                <motion.div
+                    className="ferris-wheel-intro"
+                    key={iconFilename}
+                    initial="initial"
+                    animate="animate"
+                    variants={animationVariants[index]}
+                >
+                    <div className="intro-parent">
+                        <img
+                            src={require(`../assets/img/tech_icon/${iconFilename}`)}
+                            alt={`${getTechName(iconFilename)} technology icon`}
+                            className="intro-img"
+                        />
+                    </div>
+                </motion.div>
+            ))}
         </div>
-
-
-    )
-}
+    );
+};
